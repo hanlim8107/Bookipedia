d752319046e96e363374c1dada705dd53625c566
hanlim8107 <hanlim8107@gmail.com>
2021-08-17 21:02:42 +0900

    Modify the design of the detail page

--- app/src/view/DetailView.js
+++ app/src/view/DetailView.js
@@ -9,57 +9,81 @@ import Typography from '@material-ui/core/Typography';
 
 // Styling
 const StyledGrid = styled(Grid)`
-    margin: auto;
+    margin: 70px auto !important;
+    max-width: 1000px;
 `
 
-const Summary = styled(Typography)`
-    height: 50px;
-    cursor: pointer;
+const StyledImage = styled(CardMedia)`
+    margin: auto;
+    width: 153px !important;
+    height: 221px;
+    object-fit: fill !important;
+`
 
-    overflow: ${(props) => { if (props.overflow) { return 'hidden' } else { return 'visible'} }}
+const StyledTitle = styled(Typography)`
+    margin-top: 80px !important;
+    font-size: 15px !important;
+    font-weight: bold !important;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+`
+
+const StyledAuthor = styled(Typography)`
+    font-size: 12px !important;
+    text-align: center;
+`
+
+const TextBox = styled.div`
+    margin: auto;
+    width: 500px;
 `
 
 
 export default function DetailView({detailData}) {
-    // Summary overflow Animation
-    let [overflow, setOverflow] = useState(true)
-    console.log(detailData)
 
     return (
         <div>
             {
             detailData !== undefined
 
-            ?   <StyledGrid container spacing={2}>
-                    <Grid item xs={4}>
-                        <CardMedia
-                        component="img"
-                        alt="Contemplative Reptile"
-                        height="140"
-                        image={detailData.image._text}
-                        title="Contemplative Reptile"
-                        />
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                        {detailData.title._text}
-                        </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                        {detailData.author._text}
-                        </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                        {detailData.pubdate._text}
-                        </Typography>
-                        <Summary gutterBottom variant="h5" component="h2" overflow={overflow} onClick={() => {
-                            setOverflow(!overflow)
-                        }}>
-                        {detailData.description._text}
-                        </Summary>
-                        <Typography gutterBottom variant="h5" component="h2">
-                        {detailData.publisher._text}
-                        </Typography>
-                    </Grid>
-                </StyledGrid>
+            ?   <div>
+                    <StyledGrid 
+                        container
+                        direction='row'
+                        justifyContent='center'
+                    >
+                        <Grid item xs={12} sm={3}>
+                            <StyledImage
+                            component="img"
+                            alt="Contemplative Reptile"
+                            height="140"
+                            image={detailData.image._text}
+                            title="Contemplative Reptile"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <StyledTitle gutterBottom variant="h5" component="h2">
+                            {detailData.title._text}
+                            </StyledTitle>
+                            <StyledAuthor gutterBottom variant="h5" component="h2">
+                            {detailData.author._text}
+                            </StyledAuthor>
+                        </Grid>
+                    </StyledGrid>
+                    <TextBox>
+                        <h3>상세정보</h3>
+                        <p>{detailData.description._text}</p>
+                    </TextBox>
+                    <TextBox>
+                        <h3>출판사</h3>
+                        <p>{detailData.publisher._text}</p>
+                    </TextBox>
+                    <TextBox>
+                        <h3>출간일</h3>
+                        <p>{detailData.pubdate._text}</p>
+                    </TextBox>
+                </div>
 
             :   null
             }
